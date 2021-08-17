15204064e43f5088dfcbd8a8c525fd9441aa1db6
visibiIis <visibilis@ukr.net>
2021-08-18 00:24:43 +0300

    Redux Shop- and HomePage + adding CollectionsOverview to simplify ShopPage

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,13 @@
 import React from "react";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
 import "./shop.styles.scss";
-import SHOP_DATA from "./shop.data";
 
-class ShopPage extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {
-			collections: SHOP_DATA,
-		};
-	}
-
-	render() {
-		const { collections } = this.state;
-
-		return (
-			<div className="shop-page">
-				{collections.map(({ id, ...otherCollectionProps }) => (
-					<CollectionPreview key={id} {...otherCollectionProps} />
-				))}
-			</div>
-		);
-	}
-}
+const ShopPage = ({ collections }) => (
+	<div className="shop-page">
+		<CollectionsOverview />
+	</div>
+);
 
 export default ShopPage;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,23 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer.js";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
-export default combineReducers({
+const persistConfig = {
+	key: "root",
+	storage,
+	whitelist: ["cart", "user"],
+};
+
+const rootReducer = combineReducers({
 	cart: cartReducer,
 	user: userReducer,
+	directory: directoryReducer,
+	shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
