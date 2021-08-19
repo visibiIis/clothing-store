84bfafd7bbbbb2632eb48022cae5f0cd24a130ca
visibiIis <visibilis@ukr.net>
2021-08-20 00:50:17 +0300

    Stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey =
+		"pk_test_51JQIW2DrJ9KJOVLtSzUrLxMpCNRkLCrq6rUiq4yOe6OfXAtVaYWIN751jJKa64XAfo0fZykXmM57QzLyT72cXtDU00fLeCsXct";
+
+	const onToken = (token) => {
+		console.log(token);
+		alert("Payment successful");
+	};
+
+	return (
+		<StripeCheckout
+			label="Pay Now"
+			name="CRWN Clothing Ltd."
+			billingAddress
+			shippingAddress
+			image="https://svgur.com/i/_N2.svg"
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
